14739b4ca5e02f09a359022f9f0fef0e229d339c
mj-zhang1029 <130682569+mj-zhang1029@users.noreply.github.com>
2024-02-07 22:27:38 -0500

    trying to add pick map

--- ReviewProject.java
+++ ReviewProject.java
@@ -1,9 +1,41 @@
 import arc.*;
+//import java.awt.image.BufferedImage;
 
-public class ReviewProject {
+public class ReviewProject{
     public static void main(String[] args){
         Console con = new Console();
-        con.println("hi");
-        
+        int intPickMap;
+
+        con.println("Which map would you like to pick?");
+        intPickMap = con.readInt();
+
+        while(intPickMap != 1 && intPickMap != 2){
+            con.println("There are only two maps");
+            intPickMap = con.readInt();
+        }
+        if(intPickMap == 1){
+            con.println("map 1");
+        }else if(intPickMap == 2){
+            con.println("map 2");
+        }
+
+        /*loading pictures
+        BufferedImage imgGrass;
+		imgGrass = con.loadImage("grass.png");
+        BufferedImage imgTrees;
+		imgTrees = con.loadImage("tree.png");
+        BufferedImage imgWater;
+		imgWater = con.loadImage("water.png");
+        BufferedImage imgBuild;
+		imgBuild = con.loadImage("building.png");
+        BufferedImage imgE1;
+		imgE1 = con.loadImage("enemy1.png");
+        BufferedImage imgE2;
+		imgE2 = con.loadImage("enemy2.png");
+        BufferedImage imgE3;
+		imgE3 = con.loadImage("enemy3.png");
+        BufferedImage imgH;
+		imgH = con.loadImage("hero.png");
+        */
     }
 }
