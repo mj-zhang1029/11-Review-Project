5f48f046f120ec177b7e77ab1a68b3a1510beec5
mj-zhang1029 <130682569+mj-zhang1029@users.noreply.github.com>
2024-02-09 16:32:15 -0500

    moved functions into methods for organization

--- ReviewProject.java
+++ ReviewProject.java
@@ -1,5 +1,5 @@
 import arc.*;
-import java.awt.image.BufferedImage;
+//import java.awt.image.BufferedImage;
 import java.awt.Color;
 import java.awt.Font;
 
@@ -22,15 +22,30 @@ public class ReviewProject{
 		imgBuild = con.loadImage("building.png");
         BufferedImage imgE1;
 		imgE1 = con.loadImage("enemy1.png");
-        */
+        
         BufferedImage imgE2;
 		imgE2 = con.loadImage("enemy2.png");
         BufferedImage imgE3;
 		imgE3 = con.loadImage("enemy3.png");
-        //BufferedImage imgH;
-		//imgH = con.loadImage("hero.png");
+        BufferedImage imgH;
+		imgH = con.loadImage("hero.png");
+        */
         
-
+        String strChoice;
+        strChoice = Menu(con);
+        Clear(con);
+        if(strChoice.equals("play")){
+            Play(con);
+        }else if(strChoice.equals("map")){
+            String strMapChoice;
+            strMapChoice = Map(con);
+            Clear(con);
+            Menu(con);
+            con.println(strMapChoice);
+        }  
+    }
+    //game menu
+    public static String Menu(Console con){
         //variables for buttons
 		int intMouseX = 0;
 		int intMouseY = 0;
@@ -49,6 +64,12 @@ public class ReviewProject{
 				con.setDrawColor(Color.RED);
 				con.drawString("PLAY",370,165);
                 
+                if(intMouseClick == 1){
+                    strRepeat = "no";
+
+                    return("play");
+                }
+
 				con.repaint();
 			}else if(((intMouseX >= 250) && (intMouseX <= 550)) && ((intMouseY >= 360) && (intMouseY <= 360+80))){
 				//map button
@@ -57,45 +78,8 @@ public class ReviewProject{
                 
                 if(intMouseClick == 1){
                     strRepeat = "no";
-                    Clear(con);
-                    con.setDrawColor(Color.WHITE);
-                    con.drawString("Pick Your Difficulty",275,140);
-
-                    //buttons for map selection
-                    con.fillRect(100, 300, 250, 300);
-                    con.fillRect(450, 300, 250, 300);
-                    con.setDrawColor(Color.RED);
-                    con.drawString("LVL 1",190,570);
-                    con.drawString("LVL 2",540,570);
-                    con.drawImage(imgE2,100,300);
-                    con.drawImage(imgE3,450,300);
-
-                    if(((intMouseX >= 100) && (intMouseX <=350)) && ((intMouseY >= 300) && (intMouseY <= 140+300))){
-                        //lvl 1 button
-                        con.setDrawColor(Color.RED);
-				        con.drawString("LVL 1",190,570);
-
-                        con.repaint();
-                    }else if(((intMouseX >= 450) && (intMouseX <=700)) && ((intMouseY >= 300) && (intMouseY <= 140+300))){
-                        //lvl 2 button
-                        con.setDrawColor(Color.RED);
-				        con.drawString("LVL 2",540,570);
-
-                        con.repaint();
-                    }else{
-                        //redraws buttons, words, and outline if user is not hovering button
-                        con.setDrawColor(Color.WHITE);
-                        con.fillRect(100, 300, 250, 300);
-                        con.fillRect(450, 300, 250, 300);
-                        
-                        con.setDrawColor(Color.RED);
-                        con.drawString("LVL 1",190,570);
-                        con.drawString("LVL 2",540,570);
-                        con.drawImage(imgE2,100,300);
-                        con.drawImage(imgE3,450,300);
-                        
-                        con.repaint();
-                    }
+                    
+                    return("map");
                 }
 
                 con.repaint();
@@ -125,7 +109,74 @@ public class ReviewProject{
 				con.repaint();
             }
         }
+
+        return ("hi");
     }
+    
+    //play option
+    public static void Play(Console con){
+        Clear(con);
+        con.println("woohoo game");
+    }
+
+    //map selection
+    public static String Map(Console con){
+        //variables for buttons
+		int intMouseX = 0;
+		int intMouseY = 0;
+		int intMouseClick = 0;
+		String strRepeat;
+		strRepeat = "yes";
+        
+        Clear(con);
+        con.setDrawColor(Color.WHITE);
+        con.drawString("Pick Your Difficulty",275,140);
+
+        while(strRepeat.equals("yes")){
+			//repeatedly gets x, y, and click of mouse
+			intMouseX = con.currentMouseX();
+			intMouseY = con.currentMouseY();
+			intMouseClick = con.currentMouseButton();
+			
+			if(((intMouseX >= 100) && (intMouseX <= 350)) && ((intMouseY >= 300) && (intMouseY <= 600))){
+				//play button
+				con.setDrawColor(Color.RED);
+                con.drawString("LVL 1",190,570);
+                
+                if(intMouseClick == 1){
+                    strRepeat = "no";
+
+                    return("map.csv");
+                }
+
+				con.repaint();
+			}else if(((intMouseX >= 450) && (intMouseX <= 700)) && ((intMouseY >= 300) && (intMouseY <= 600))){
+				//map button
+				con.setDrawColor(Color.RED);
+                con.drawString("LVL 2",540,570);
+                
+                if(intMouseClick == 1){                    
+                    strRepeat = "no";
+
+                    return("map2.csv");
+                }
+
+                con.repaint();
+			}else{
+				//redraws buttons, words, and outline if user is not hovering button
+				con.setDrawColor(Color.WHITE);
+                con.fillRect(100, 300, 250, 300);
+                con.fillRect(450, 300, 250, 300);
+                con.setDrawColor(Color.RED);
+                con.drawString("LVL 1",190,570);
+                con.drawString("LVL 2",540,570);
+								
+				con.repaint();
+            }
+        }
+        return("map.csv");
+    }
+
     //clears screen
 	public static void Clear(Console con){
 		con.sleep(500);
@@ -133,4 +184,5 @@ public class ReviewProject{
 		con.fillRect(0,0,1280,720);
 		con.clear();
 	}
+
 }
